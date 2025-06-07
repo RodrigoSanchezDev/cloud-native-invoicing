f33cf59b5419bb4a4d9695f7135e7f0a8993456a
RodrigoSanchez35 <rodr.sanchezc@duocuc.cl>
2025-06-07 15:38:43 -0600

    Fix compilation issues and prepare for deployment

--- file-service/src/main/java/com/sanchezdev/fileservice/config/S3Config.java
+++ file-service/src/main/java/com/sanchezdev/fileservice/config/S3Config.java
@@ -1,37 +1,7 @@
 package com.sanchezdev.fileservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("${spring.cloud.aws.credentials.access-key}")
-    private String accessKeyId;
-    @Value("${spring.cloud.aws.credentials.secret-key}")
-    private String secretKey;
-    @Value("${spring.cloud.aws.credentials.session-token}")
-    private String sessionToken;
-    @Value("${spring.cloud.aws.region.static}")
-    private String region;
-
-    @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                    StaticCredentialsProvider.create(
-                        AwsSessionCredentials.create(
-                            accessKeyId,
-                            secretKey,
-                            sessionToken
-                        )
-                    )
-                )
-                .build();
-    }
 }
