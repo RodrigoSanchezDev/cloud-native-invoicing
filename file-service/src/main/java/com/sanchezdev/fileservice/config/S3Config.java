e5f7e86c0c6fde390535359e8256cdfb7ec2a24a
RodrigoSanchez35 <rodr.sanchezc@duocuc.cl>
2025-06-08 15:27:48 -0600

    Fix compilation issues and prepare for deployment

--- file-service/src/main/java/com/sanchezdev/fileservice/config/S3Config.java
+++ file-service/src/main/java/com/sanchezdev/fileservice/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -14,11 +15,20 @@ public class S3Config {
   public S3Client s3Client(
       @Value("${spring.cloud.aws.credentials.access-key}") String accessKey,
       @Value("${spring.cloud.aws.credentials.secret-key}") String secretKey,
-      @Value("${spring.cloud.aws.region.static}") String region) {
-    return S3Client.builder()
-        .region(Region.of(region))
-        .credentialsProvider(StaticCredentialsProvider.create(
-            AwsBasicCredentials.create(accessKey, secretKey)))
-        .build();
+      @Value("${spring.cloud.aws.region.static}") String region,
+      @Value("${AWS_SESSION_TOKEN:}") String sessionToken) {
+    if (sessionToken != null && !sessionToken.isEmpty()) {
+      return S3Client.builder()
+          .region(Region.of(region))
+          .credentialsProvider(StaticCredentialsProvider.create(
+              AwsSessionCredentials.create(accessKey, secretKey, sessionToken)))
+          .build();
+    } else {
+      return S3Client.builder()
+          .region(Region.of(region))
+          .credentialsProvider(StaticCredentialsProvider.create(
+              AwsBasicCredentials.create(accessKey, secretKey)))
+          .build();
+    }
   }
 }
